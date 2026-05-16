6ccbd2b601439e133ae25adc7669314952b7a21c
MazenTurk201 <maznktr@gmail.com>
2026-05-16 03:48:46 +0300

    33

--- vite.config.ts
+++ vite.config.ts
@@ -11,4 +11,14 @@ export default defineConfig({
     tsconfigPaths() // الإضافة دي هتربط الـ الـ الـ aliases بتاعة الـ tsconfig بـ Vite علطول
   ],
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        // السطور دي بتجبر فيت يسمي الملفات بـ .js صريحة عشان جيتهب ميتلغبطش في الـ MIME type
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
 })
